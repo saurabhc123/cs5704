facb56682447e8636d94ad97b966ffdb678b483a
Saurabh Chakravarty <saurabc@vt.edu>
2019-12-07 12:26:35 -0500

    Work in progress.

--- Implementations/Serializers/csv_file_serializer.py
+++ Implementations/Serializers/csv_file_serializer.py
@@ -1,3 +1,6 @@
+import os
+import pickle
+
 from Framework.serializer import Serializer
 
 
@@ -6,6 +9,7 @@ class CsvFileSerializer(Serializer):
     def __init__(self, folder, file_name):
         self.file_name = file_name
         self.folder = folder
+        self.buffer = []
         pass
 
     def serialize(self, edge_list):
@@ -24,12 +28,15 @@ class CsvFileSerializer(Serializer):
                 self.write_header(left_revision_string, right_revision_string)
             self.write_edge(edge_list[i][0].line_number, edge_list[i][2], edge_list[i][1].line_number)
 
+        full_file_path = os.path.join(self.folder, self.file_name)
+        with open(full_file_path, 'w') as filehandle:
+            filehandle.writelines("%s\n" % line for line in self.buffer)
 
     def deserialize(self, output_data_bag):
         pass
 
     def write_header(self, left_revision_string, right_revision_string):
-        pass
+        self.buffer.append("{},{}".format(left_revision_string, right_revision_string))
 
-    def write_edge(self, line_number, param, line_number1):
-        pass
+    def write_edge(self, left_line_number, param, right_line_number):
+        self.buffer.append("{},{},{}".format(left_line_number, param, right_line_number))
